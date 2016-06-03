266da10682ffcd5084846df016666a4f113fb102
AlexMehari <alex@Mac-Mill.local>
2016-06-03 18:11:05 +0200

    Eerste stabiele versie
    Winnaar bij 1000 punten.
    Haai geluid moet aangepast worden.

---
+++ clegames/Development/2 - Fish Game/Fish Game 3.0/dev/playerfish.ts
@@ -0,0 +1,106 @@
+/// <reference path="gameobject.ts" />
+/**
+ * Player
+ */
+class Player extends GameObject {
+    private downKey : number;
+    private upKey : number;
+    private leftKey : number;
+    private rightKey : number;
+    
+    private downSpeed : number = 0;
+    private upSpeed : number = 0;
+    private leftSpeed : number = 0;
+    private rightSpeed : number = 0;
+    private speed : number = 1;
+    
+    private player : number;
+    
+    constructor(l:Level, x:number, y:number, width:number, height:number, tagName:string, player:number) {
+        super(l, x, y, width, height, tagName);
+        
+        if(player==1){
+            this.upKey=87;
+            this.downKey=83;
+            this.leftKey=65;
+            this.rightKey=68;
+        }
+        else{
+            
+            this.upKey = 38;
+            this.downKey = 40;
+            this.leftKey = 37;
+            this.rightKey = 39;
+        }
+        
+        window.addEventListener("keydown", this.onKeyDown.bind(this));
+        window.addEventListener("keyup", this.onKeyUp.bind(this));
+        
+        this.player = player;
+    }
+    
+    private onKeyDown(event:KeyboardEvent):void {
+        switch(event.keyCode){
+        case this.upKey:
+            this.upSpeed = -7.5;
+            break;
+        case this.downKey:
+            this.downSpeed = 7.5;
+            break;
+        case this.leftKey:
+            this.leftSpeed = -10;
+            this.setDirection(-1);
+            break;
+        case this.rightKey:
+            this.rightSpeed = 10;
+            this.setDirection(1);
+            break;
+        }
+    }
+
+    private onKeyUp(event:KeyboardEvent):void {
+        switch(event.keyCode){
+        case this.upKey:
+            this.upSpeed = 0;
+            break;
+        case this.downKey:
+            this.downSpeed = 0;
+            break;
+        case this.leftKey:
+            this.leftSpeed = 0;
+            break;
+        case this.rightKey:
+            this.rightSpeed = 0;
+            break;
+        }
+    }
+    
+    public update() : void {   
+        if(this.x+this.leftSpeed<=0){
+            this.leftSpeed=0;
+        }
+        else if(this.x+this.rightSpeed>=innerWidth-this.width){
+            this.rightSpeed=0;
+        }
+        else if(this.y+this.upSpeed<=0){
+            this.upSpeed=0;
+        }
+        else if(this.y+this.downSpeed>=innerHeight-this.height-10){
+            this.downSpeed=0;
+        }
+        
+        this.x+=(this.leftSpeed+this.rightSpeed)*this.speed;
+        this.y+=(this.upSpeed+this.downSpeed)*this.speed;    
+        
+        if(this.speed>1.75) this.speed-=0.001;
+        else if(this.speed>1) this.speed-=0.1;        
+    }
+    
+    public speedUp() : void{
+        this.speed=2;
+    }
+    
+    public getPlayer() : number{
+        return this.player;
+    }
+}
---
+++ clegames/Development/2 - Fish Game/Fish Game 3.0/dev/scoredisplay.ts
@@ -0,0 +1,57 @@
+/**
+ * ScoreDisplay
+ */
+class ScoreDisplay {
+    private score:number = 0;
+    private s1:number=0;
+    private s2:number=0;
+    private level:Level;
+    private div : Element;
+    private div2 : Element;
+    private div3 : Element;
+    
+    constructor(l:Level) {
+        this.div = document.getElementsByTagName("ui")[0];
+        this.div2 = document.getElementsByTagName("ui2")[0];
+        this.div3 = document.getElementsByTagName("ui3")[0];
+        this.level = l;
+        this.div.innerHTML = "0";
+        this.div2.innerHTML = "0";  
+    }
+    
+    public updateScores(score:number, player:number){
+        if(player==1){
+            this.s1+=score;
+        }
+        else{
+            this.s2+=score;
+        }
+        this.display();
+        this.gameOver();
+    }
+    
+    public getScores(player:number):number{
+        if(player==1){
+            return this.s1
+        }
+        else{
+            return this.s2;
+        }
+    }
+    
+    private display(){
+         this.div.innerHTML = ""+Math.floor(this.s1);
+         this.div2.innerHTML = ""+Math.floor(this.s2);
+    }
+    
+    private gameOver(){
+        if(this.s1>=1000){
+            this.div3.innerHTML = "Speler 1 heeft gewonnen!";
+            this.level.setGameOver(true);
+        }
+        else if(this.s2>=1000){
+            this.div3.innerHTML = "Speler 2 heeft gewonnen!";
+            this.level.setGameOver(true);
+        }
+    }
+}
