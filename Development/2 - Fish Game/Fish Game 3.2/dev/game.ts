7f48b9d393e3880ab3348cb11051f2f95183d33e
AlexMehari <alex@Mac-Mill.local>
2016-06-06 18:27:10 +0200

    Power-up effect veranderd
    Klok power-up verhoogt tijdelijk de snelheid van de speler die het pakt
    en verlaagt nu ook de snelheid van de andere speler tijdelijk

---
+++ Development/2 - Fish Game/Fish Game 3.2/dev/game.ts
@@ -0,0 +1,19 @@
+/// <reference path="level.ts"/>
+class Game { 
+    private level:Level;
+     
+    constructor() {
+        this.level = new Level(this);
+        
+        requestAnimationFrame(this.gameLoop.bind(this));
+    }
+    
+    private gameLoop(){ 
+        this.level.update();
+        this.level.draw();
+                
+        // hiermee wordt de gameloop opnieuw aangeroepen
+        requestAnimationFrame(this.gameLoop.bind(this));
+    }
+} 
+
